a67014046c8e93aa1e2fd154ff01ad0fe2c41835
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-02-12 23:44:52 +0100

    basic auth implementation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,13 +14,29 @@ class Auth:
         """
         checks if path require auth
         """
+        if path is None or excluded_paths is None:
+            return True
+
+        if path[-1] != '/':
+            path_2 = path + '/'
+        else:
+            path_2 = path[:-2]
+
+        if path not in excluded_paths and path_2 not in excluded_paths:
+            return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
         """
         gets the authorization header
         """
-        return None
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """fetches the current user"""
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+the auth module
+"""
+import base64
+import binascii
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic Auth class"""
+
+    def extract_base64_authorization_header(self, auth_header: str) -> str:
+        """
+        fetches and returns the authorization header
+        """
+        if auth_header is None or not isinstance(auth_header, str):
+            return None
+        if not auth_header.startswith('Basic '):
+            return None
+
+        return auth_header.lstrip('Basic ')
+
+    def decode_base64_authorization_header(self, base64_header: str) -> str:
+        """
+        decodes and return encoded
+        Authorization header
+        """
+        if base64_header is None or not isinstance(base64_header, str):
+            return None
+
+        try:
+            return base64.b64decode(base64_header).decode('utf-8')
+        except binascii.Error:
+            return None
+
+
+    def extract_user_credentials(self, auth_header: str) -> (str, str):
+        """
+        return the decoded credentials
+        """
+        if auth_header is None or not isinstance(auth_header, str):
+            return None
+        if not ':' in auth_header:
+            return None
+
+        username, password = auth_header.split(':')
+        return username, password
