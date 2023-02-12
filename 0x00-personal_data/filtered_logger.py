cce6d3062140fcf68a739da815c12b089d89204b
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-02-12 12:17:31 +0100

    function to create a custom logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ import logging
 from typing import List
 
 
-PII_FIELDS = ('email', 'phone', 'ssn', 'password', 'ip',)
+PII_FIELDS = ('name', 'phone', 'ssn', 'password', 'ip',)
 
 
 def filter_datum(fields: List[str], redaction: str,
