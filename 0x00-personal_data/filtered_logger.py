343cbdcb151c02acab265eea3510a8ee62a6295b
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-02-12 15:32:37 +0100

    connects to db and log

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -94,9 +94,9 @@ def main():
     cursor.execute("SELECT * FROM users;")
     for user in cursor:
         msg = (
-            f"name={user[0]};email={user[1]};phone={user[2]};"
-            f"ssn={user[3]};password={user[4]};ip={user[5]};"
-            f"last_login={user[6]};user_agent={user[7]};"
+            f"name={user[0]}; email={user[1]}; phone={user[2]}; "
+            f"ssn={user[3]}; password={user[4]}; ip={user[5]}; "
+            f"last_login={user[6]}; user_agent={user[7]};"
         )
 
         logger.info(msg)
