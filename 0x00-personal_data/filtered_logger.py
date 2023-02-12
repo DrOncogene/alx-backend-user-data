08b6c17dfd6d1716ff067d821c00130d32ac7209
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-02-12 09:33:07 +0100

    a redacting formatter class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 filters and obfuscate fields from a message
 """
 import re
+import logging
 from typing import List
 
 
@@ -27,3 +28,23 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(regex, redaction, message)
 
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        message = super().format(record)
+        return message
