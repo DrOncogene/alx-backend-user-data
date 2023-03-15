58ff9589f0c771657298f4f6b9df8be0e8659d09
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-03-15 22:14:41 +0100

    view to implement session login

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+session auth view
+"""
+from os import getenv
+
+from api.v1.views import app_views
+from flask import jsonify, request
+
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'],
+                 strict_slashes=False)
+def session_auth():
+    """handles login with session id"""
+    email = request.form.get('email', None)
+    password = request.form.get('password', None)
+
+    if email is None:
+        return jsonify({"error": "email missing"}), 400
+    if password is None:
+        return jsonify({'error': 'password missing'}), 400
+    
+    user = User.search({'email': email})
+    if len(user) == 0:
+        return jsonify({'error': 'no user found for this email'}), 404
+    if not user[0].is_valid_password(password):
+        return jsonify({'error': 'wrong password'}), 401
+
+    from api.v1.app import auth
+    sid = auth.create_session(user[0].id)
+    session_name = getenv('SESSION_NAME', None)
+
+    response = jsonify(user.to_json())
+    response.set_cookie(session_name, sid)
+
+    return response
