1db40a988fb62f719f4a06097c62e400ab8d6935
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-03-15 10:54:55 +0100

    in-memory session store

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,8 @@
 """
 session auth
 """
+from uuid import uuid4
+
 from .auth import Auth
 
 
@@ -9,3 +11,13 @@ class SessionAuth(Auth):
     """
     Session auth class
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        sid = str(uuid4())
+        self.user_id_by_session_id[sid] = user_id
+
+        return sid
