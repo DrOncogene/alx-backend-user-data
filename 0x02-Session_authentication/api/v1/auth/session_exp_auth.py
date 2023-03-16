aedba1f90187f214597523b75bc6ac08f0a40188
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-03-16 08:25:03 +0100

    session login with expiration

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+session login with expiration
+"""
+from os import getenv
+from datetime import datetime
+
+from .session_auth import SessionAuth
+
+
+class SessionExpAuth(SessionAuth):
+    """expiring session auth class"""
+    def __init__(self) -> None:
+        self.session_duration = int(getenv('SESSION_DURATION') or 0)
+        super().__init__()
+
+    def create_session(self, user_id: str = None) -> str:
+        sid = super().create_session(user_id)
+        if sid is None:
+            return None
+
+        self.user_id_by_session_id[sid] = {
+            'user_id': user_id,
+            'created_at': datetime.now()
+        }
+        return sid
+
+    def user_id_for_session_id(self, sid: str = None) -> str:
+        """fetches user data from session"""
+        if sid is None:
+            return None
+        if not self.user_id_by_session_id.get(sid, None):
+            return None
+
+        session_dict = self.user_id_by_session_id[sid]
+        if self.session_duration <= 0:
+            return session_dict.get('created_at', None)
+
+        created = session_dict['created_at'].timestamp()
+        if datetime.now().timestamp() > created + self.session_duration:
+            return None
+
+        return session_dict['user_id']
