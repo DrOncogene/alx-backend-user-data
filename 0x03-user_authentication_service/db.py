ec76c9f2d5ff0925afd6014e0e5df39bdc4dc667
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-03-20 21:17:20 +0100

    moved the User class import to the top

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -34,9 +34,8 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """creates a new user"""
-        from user import User
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
