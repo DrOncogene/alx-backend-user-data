f494e6717751e48703053d8af474f030d89a86ff
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-03-21 12:44:10 +0100

    app route to delete session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 basic flask app
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect, url_for
 
 from auth import Auth
 
@@ -45,6 +45,18 @@ def login():
     response.set_cookie('session_id', session_id)
 
     return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """logs a user in"""
+    sid = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(sid)
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+    return redirect(url_for('/'))
 
 
 if __name__ == "__main__":
