0dc0ee64cde9b9e696718fa0e1471e394bd0315c
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-03-21 09:26:20 +0100

    validating user password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -38,12 +38,12 @@ class Auth:
 
         return self._db.add_user(email, passwd)
 
-    def valid_login(self, email: str, passwd: str) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """
         validates the user password
         """
         try:
             user = self._db.find_user_by(email=email)
-            return checkpw(passwd.encode('utf-8'), user.hashed_password)
+            return checkpw(password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
