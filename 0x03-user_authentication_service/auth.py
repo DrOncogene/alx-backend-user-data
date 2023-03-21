1a4308b8e271ef8f2ede7133249ce45da5e7f49e
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-03-21 12:11:07 +0100

    function to delete a session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,3 +81,11 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """deletes a session"""
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return
