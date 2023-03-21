efb8e236a1b4451641bb814948b958138fa28adf
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-03-21 08:24:37 +0100

    authentication class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,11 @@
 """
 auth module
 """
+from sqlalchemy.exc import NoResultFound
 from bcrypt import hashpw, gensalt
+
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,3 +17,23 @@ def _hash_password(password: str) -> bytes:
     hashed = hashpw(password.encode('utf-8'), salt)
 
     return hashed
+
+
+class Auth:
+    """Auth class"""
+    def __init__(self) -> None:
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        adds a new user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            pass
+
+        passwd = _hash_password(password)
+
+        return self._db.add_user(email, passwd)
